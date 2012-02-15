c6e334d0a9be2bec8acedefc4c4a160b13f4308d
xtang123 <xaviertang.email@gmail.com>
2012-02-15 11:27:15 +0100

    Update Projet-s4/AlgoAleatoires/repartitionClientsResto.php

--- Projet-s4/AlgoAleatoires/repartitionClientsResto.php
+++ Projet-s4/AlgoAleatoires/repartitionClientsResto.php
@@ -1,17 +1,11 @@
 <?php
 
 //retourne un tableau d'entier contenant le nombre de clients dans chaque restaurant de la base de données
-
-/* Auteur Maxence Xavier Thomas */
-
 function repartitionClientsResto() {	
-	$nbRestau=nbTotalResto(); //On recupère le nombre de restaurant
-
-	for($i=0; $i<$nbRestau; $i++){  // pour chaque ID restaurant
-		$tabNbClientResto[i]=nbClientsResto($i);  // on récupère son nombre de clients via la fonction nbClientsResto
+	$result = mysql_query ("SELECT id FROM restaurant");
+	while ($row = mysql_fetch_object ($result)) {
+		nbClientsResto($row[0]);  // on récupère son nombre de clients via la fonction nbClientsResto
 	}
-	
-	return $tabNbClientResto;  // on retourne le tableau
 }
 
 ?>
