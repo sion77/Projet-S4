0a1929a16bbc6c50f9500b8a519b1916394c20b5
Thomas L. <levasseur.tom@gmail.com>
2012-02-15 11:50:46 +0100

    Update Projet-s4/AlgoAleatoires/connexionDB.php

--- Projet-s4/AlgoAleatoires/connexionDB.php
+++ Projet-s4/AlgoAleatoires/connexionDB.php
@@ -1,29 +1,20 @@
 <?php
-	//Constantes
-	define ('HOST', 'localhost');
-	define ('USER', 'root');
-	define ('PASSWORD', '');
-	define ('DB', 'restaugame');
 	
-	/* Connexion à la base de données. Renvoie l'id de connexion.
-	* Auteur Julien
-	*/
-	function connexionDB() {
-		//Connexion au serveur
-		$idConnexion = mysql_connect(HOST, USER, PASSWORD);
-		
-		if(!$idConnexion)
-			//En cas d'erreur avec le serveur
-			echo "Attention : problème de connexion avec le serveur.";
-		else {
-			//Connexion à la base de données
-			$connexionReussie = mysql_select_db(DB, $idConnexion);
-			
-			if(!$connexionReussie)
-				//En cas d'erreur avec la base de données
-				echo "Attention : problème de connexion à la base de données.";
-				
-			return $idConnexion;
-		}
+	try 
+	{
+		//$host = "localhost";
+		// include(connexionBD.php);
+		// GÃ©nÃ©ralement la machine est localhost  
+		// c'est-a-dire la machine sur laquelle le script est hÃ©bergÃ©
+		$user = "root";
+		//$bdd = "maBase";
+		$passwd = "mysql";
+		$pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
+		$co = new PDO ('mysql:host=localhost;dbname=maBase', $user , $passwd, $pdo_options);
+	}
+	
+	catch(Exception $e)
+	{
+		die('Erreur : '.$e->getMessage());
 	}
 ?>
