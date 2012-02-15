0fd4ad2f99cdc870d596975c6eed8f26dd60b217
Thomas L. <levasseur.tom@gmail.com>
2012-02-15 11:53:36 +0100

    Update Projet-s4/AlgoAleatoires/repartitionClientsResto.php

--- Projet-s4/AlgoAleatoires/repartitionClientsResto.php
+++ Projet-s4/AlgoAleatoires/repartitionClientsResto.php
@@ -2,7 +2,7 @@
 
 //retourne un tableau d'entier contenant le nombre de clients dans chaque restaurant de la base de données
 function repartitionClientsResto() {	
-	$result = $bdd-> query ('SELECT id FROM restaurant');
+	$result = $co-> query ('SELECT id FROM restaurant');
 	while ($row = $result->fetch()) {
 		nbClientsResto($row['id']);  // on récupère son nombre de clients via la fonction nbClientsResto
 	}
