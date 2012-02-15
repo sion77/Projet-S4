417cc2fa16c27f976736c5f9cea6e3c0c38be71c
Julien Rene <julien.rene@u-psud.fr>
2012-02-15 12:01:11 +0100

    Update qualitePrestation.php (L'impact est probablement trop important sur la note. A tester)

--- Projet-s4/AlgoAleatoires/qualitePrestation.php
+++ Projet-s4/AlgoAleatoires/qualitePrestation.php
@@ -7,37 +7,34 @@
 		//Le salaire de référence est une variable qui est utilisé pour relativiser le salaire de l'employé par rapport au restaurant dans lequel il travaille.
 		$salaireReference = 0;
 		$moy = 0;
+		$qualitePrestation = 0.7;
 		$idConnexion = connexionDB();
 		
 		//Requête qui récupère l'id du restaurant de l'employé
-		$result = mysql_query("SELECT idRestaurant FROM employe WHERE id = " . $unSalarie, $idConnexion);
+		$requete = mysql_query("SELECT idRestaurant FROM employe WHERE id = " . $unSalarie, $idConnexion);
 		//Requête qui récupère le salaire de l'employé
-		$result2 = mysql_query("SELECT salaire FROM employe WHERE id = " . $unSalarie, $idConnexion);
+		$requete2 = mysql_query("SELECT salaire FROM employe WHERE id = " . $unSalarie, $idConnexion);
 		
 		if(!$result)
 			die("Requête invalide : " . mysql_error());
 		else {
-			while ($row = mysql_fetch_row()) {
-				//On récupère la moyenne des menus du restaurant
-				$moy = moyenneMenu($row[0]);
+			//On récupère la moyenne des menus du restaurant
+			$moy = mysql_fetch_row($requete);
 				
-				//Détermination d'un salaire de référence en fonction du "standing" des menus du restaurant
-				if ($moy < 2.5)
-					$salaireReference = 1000;
-				else if ($moy >= 2.5 && $note < 5)
-					$salaireReference = 1300;
-				else if ($moy >= 5 && $note < 7.5)
-					$salaireReference = 1600;
-				else
-					$salaireReference = 2000;
-			}
+			//Détermination d'un salaire de référence en fonction du "standing" des menus du restaurant
+			if ($moy <= 2)
+				$salaireReference = 1000;
+			else if ($moy > 2 && $qualitePrestationote <= 4)
+				$salaireReference = 1300;
+			else if ($moy > 4 && $qualitePrestationote <= 8)
+				$salaireReference = 1600;
+			else
+				$salaireReference = 2000;
 		}
 		if(!$result2)
 			die("Requête invalide : " . mysql_error());
 		else
-			while ($row2 = mysql_fetch_row()) {
-				$salaire = $row2[0];
-			}
+			$salaire = mysql_fetch_row($requete2);
 		}
 		
 		//Détermination du minimum et du maximum pour le pourboire
@@ -46,11 +43,20 @@
 		//Détermination aléatoire du pourboire
 		$bonusPourboire = rand($minPourboire, $maxPourboire);
 		
-		//Formule potentielle de la qualité de la prestation (temporaire)
-		$n = ($salaire + $bonusPourboire) / $salaireReference;
+		//Calculs successifs du coefficient. L'efficacité du salaire diminue à chaque intervalle.
+		//Premier intervalle : Grande efficacité et on comble facilement le malus.
+		if ($moy > 2)
+			$qualitePrestation = (($salaire + $bonusPourboire) / ($salaireReference * 5)) + $qualitePrestation;
+		if ($moy > 4)
+			$qualitePrestation = (($salaire + $bonusPourboire) / ($salaireReference * 10)) + $qualitePrestation;
+		if ($moy > 6)
+			$qualitePrestation = (($salaire + $bonusPourboire) / ($salaireReference * 15)) + $qualitePrestation;
+		//Dernier intervalle : Efficacité limitée mais toujours existante
+		if ($moy > 8)
+			$qualitePrestation = (($salaire + $bonusPourboire) / ($salaireReference * 30)) + $qualitePrestation;
 		
 		//Mise à jour dans la table employe
-		$maj = mysql_query("UPDATE employe SET qualitePrestation = " . $n . " WHERE id = " $unSalarie, $idConnexion);
+		$maj = mysql_query("UPDATE employe SET qualitePrestation = " . $qualitePrestation . " WHERE id = " $unSalarie, $idConnexion);
 		if(!$maj)
 			die("Requête invalide : " . mysql_error());
 	}
