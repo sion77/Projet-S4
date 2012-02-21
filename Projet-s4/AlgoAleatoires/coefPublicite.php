2c80f80e3655ce92fd996b87e0244b1d64e805be
Julien Rene <julien.rene@u-psud.fr>
2012-02-21 12:20:04 +0100

    [0; 1] Le coefficient n'est pas encore totalement au point.

--- Projet-s4/AlgoAleatoires/coefPublicite.php
+++ Projet-s4/AlgoAleatoires/coefPublicite.php
@@ -1,34 +1,45 @@
-<?php
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr">
+	<head>
+		<meta http-equiv="Content-Type" content="application/xhtml+xml; charset=ISO-8859-1" />
+		<title>Bonjour</title>
+	</head>
+	<body>
+		<!-- DEBUT DE CODE -->
+		<p><?php
 	/* Calcule le coefficient de publicite (supérieure à 0.8) en fonction des dépenses en publicité. Cette valeur intervient dans la note du restaurant.
 	* Auteur Julien
 	*/
-	function coefPublicite($unResto){
+	//function coefPublicite($unResto){
 		//Le coefficient aura une valeur initiale pour les restaurants n'ayant pas dépensé de l'argent dans la publicité.
 		//Etant inférieur à 1, il s'agit donc d'un malus si aucune communication n'est faite.
-		$coefPublicite = 0.8;
+		//Il s'agit aussi d'une valeur par défaut s'il y a une erreur dans la requête.
+		$coefPublicite = 0.3;
 		$depenses = 0;
-		$idConnexion = connexionDB();
+		$n = 1;
+		//$idConnexion = connexionDB();
 		
 		//On récupère la valeur de la dépense en communication du restaurant
-		$requete = mysql_query("SELECT publicite FROM restaurant WHERE id = " . $unResto, $idConnexion);
-		if (!$requete)
-			die("Requête invalide : " . mysql_error());
-		else {
-			$depenses = mysql_fetch_row($requete);
+		//$requete = mysql_query("SELECT publicite FROM restaurant WHERE id = " . $unResto, $idConnexion);
+		//if (!$requete)
+			//die("Requête invalide : " . mysql_error());
+		//else {
+			//$depenses = mysql_fetch_row($requete);
+			$depenses = 70000;
 			
-			//Calculs successifs du coefficient. L'efficacité des dépenses en publicité diminue à chaque intervalle, jusqu'à avoir un effet limité au delà de 50 000 euros.
-			//Premier intervalle : Grande efficacité et on comble facilement le malus.
-			if ($depenses > 0)
-				$coefPublicite = ($depenses / 40000) + $coefPublicite;
-			if ($depenses > 10000)
-				$coefPublicite = (($depenses - 10000) / 100000) + $coefPublicite;
-			if ($depenses > 25000)
-				$coefPublicite = (($depenses - 25000) / 200000) + $coefPublicite;
-			//Dernier intervalle : Efficacité limitée mais toujours existante
-			if ($depenses > 50000)
-				$coefPublicite = (($depenses - 50000) / 1000000) + $coefPublicite;
-				
-			return $coefPublicite;
-		}
-	}
+			$coefPublicite = 0;
+			//
+			$n = floor($depenses / 1000);
+			
+			//Calculs successifs du coefficient. L'efficacité des dépenses en publicité diminue à chaque itération de la boucle.
+			for ($i = 1; $i < $n; $i++) {
+				$coefPublicite = pow($depenses / (pow(2, $i) * $depenses), 1.4) + $coefPublicite;
+			}
+			
+			echo $coefPublicite;
+		//}
+	//}
 ?>
+		</p>
+		<!-- FIN DE CODE ?-->
+	</body>
+</html>
--- Projet-s4/AlgoAleatoires/qualitePrestation.php
+++ Projet-s4/AlgoAleatoires/qualitePrestation.php
@@ -4,34 +4,13 @@
 	*/
 	function qualitePrestation($unSalarie) {
 		$salaire = 0;
-		//Le salaire de référence est une variable qui est utilisé pour relativiser le salaire de l'employé par rapport au restaurant dans lequel il travaille.
-		$salaireReference = 0;
-		$moy = 0;
-		$qualitePrestation = 0.8;
+		$qualitePrestation = 0.5;
 		$idConnexion = connexionDB();
 		
-		//Requête qui récupère l'id du restaurant de l'employé
-		$requete = mysql_query("SELECT idRestaurant FROM employe WHERE id = " . $unSalarie, $idConnexion);
 		//Requête qui récupère le salaire de l'employé
 		$requete2 = mysql_query("SELECT salaire FROM employe WHERE id = " . $unSalarie, $idConnexion);
 		
-		if(!$requete)
-			die("Requête invalide : " . mysql_error());
-		else {
-			//On récupère la moyenne des menus du restaurant
-			$moy = mysql_fetch_row($requete);
-				
-			//Détermination d'un salaire de référence en fonction du "standing" des menus du restaurant
-			if ($moy <= 2)
-				$salaireReference = 1000;
-			else if ($moy > 2 && $qualitePrestationote <= 4)
-				$salaireReference = 1300;
-			else if ($moy > 4 && $qualitePrestationote <= 8)
-				$salaireReference = 1600;
-			else
-				$salaireReference = 2000;
-		}
-		if(!$requete)
+		if(!$requete2)
 			die("Requête invalide : " . mysql_error());
 		else
 			$salaire = mysql_fetch_row($requete2);
@@ -43,17 +22,14 @@
 		//Détermination aléatoire du pourboire
 		$bonusPourboire = rand($minPourboire, $maxPourboire);
 		
-		//Calculs successifs du coefficient. L'efficacité du salaire diminue à chaque intervalle.
-		//Premier intervalle : Grande efficacité et on comble facilement le malus.
-		if ($moy > 2)
-			$qualitePrestation = (($salaire + $bonusPourboire) / ($salaireReference * 5)) + $qualitePrestation;
-		if ($moy > 4)
-			$qualitePrestation = (($salaire + $bonusPourboire) / ($salaireReference * 10)) + $qualitePrestation;
-		if ($moy > 6)
-			$qualitePrestation = (($salaire + $bonusPourboire) / ($salaireReference * 15)) + $qualitePrestation;
-		//Dernier intervalle : Efficacité limitée mais toujours existante
-		if ($moy > 8)
-			$qualitePrestation = (($salaire + $bonusPourboire) / ($salaireReference * 30)) + $qualitePrestation;
+		$qualitePrestation = 0;
+		//On récupère la partie entière du salaire divisé par 100, pour déterminer la puissance n dans la suite 1/2^n.
+		$n = floor($salaire / 100);
+		
+		//Calculs successifs du coefficient. L'efficacité de la prestation du salarié diminue à chaque itération de la boucle.
+		for ($i = 1; $i < $n; $i++) {
+			$qualitePrestation = pow($salaire / (pow(2, $i) * $salaire), 1.4) + $qualitePrestation;
+		}
 		
 		//Mise à jour dans la table employe
 		$maj = mysql_query("UPDATE employe SET qualitePrestation = " . $qualitePrestation . " WHERE id = " $unSalarie, $idConnexion);
