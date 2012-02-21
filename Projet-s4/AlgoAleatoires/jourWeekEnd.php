d5e0b49aec10b745a72da19ac4db2b477ee87789
Thomas L. <levasseur.tom@gmail.com>
2012-02-21 11:28:19 +0100

    Update Projet-s4/AlgoAleatoires/jourWeekEnd.php

--- Projet-s4/AlgoAleatoires/jourWeekEnd.php
+++ Projet-s4/AlgoAleatoires/jourWeekEnd.php
@@ -1,5 +1,8 @@
 <?php
-
+/*
+   Fonction qui permet de savoir que le jour actuel est un week-end (à modifier)
+   Auteurs Maxence,Thomas,Xavier
+*/
 function jourWeekEnd($date){
 	$weekend=$co->query('SELECT weekend FROM jourSemaine WHERE date='.$date); 
 	$weekendResu=$weekend->fetch();
