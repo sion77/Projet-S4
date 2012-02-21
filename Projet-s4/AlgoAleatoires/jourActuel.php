266acf22b047357d9f2a6dd8bd683934ca9a70d8
Thomas L. <levasseur.tom@gmail.com>
2012-02-21 11:31:35 +0100

    Update Projet-s4/AlgoAleatoires/jourActuel.php

--- Projet-s4/AlgoAleatoires/jourActuel.php
+++ Projet-s4/AlgoAleatoires/jourActuel.php
@@ -1,4 +1,9 @@
 <?php
+
+/*
+   Permet d'obtenir le jour actuel (à modifier peut-être)
+   Auteurs Xavier,Maxence,Thomas
+*/
 function jourActuel(&$leJour, &$leMois){
 	$requete=mysql_query('SELECT jourActuel, moisActuel FROM jourActuel'); 
 	$resultat = mysql_fetch_row($requete);
