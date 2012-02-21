109e58cb45242e8024e6cc2dd586868534453b6d
Thomas L. <levasseur.tom@gmail.com>
2012-02-21 11:25:34 +0100

    Update Projet-s4/AlgoAleatoires/nbTotalResto.php

--- Projet-s4/AlgoAleatoires/nbTotalResto.php
+++ Projet-s4/AlgoAleatoires/nbTotalResto.php
@@ -1,6 +1,9 @@
 <?php
 
-// retourne le nombre total de restaurant présent dans la base de données
+/*
+     Retourne le nombre total de restaurant présent dans la base de données
+     Auteur Xavier,Thomas,Maxence
+*/
 function nbTotalResto(){
 	$nbTotal=mysql_query('SELECT Count(*) FROM restaurant'); // compte le nombre de restaurant.
 	$nbTotalResu=mysql_fetch_row($nbTotal);
