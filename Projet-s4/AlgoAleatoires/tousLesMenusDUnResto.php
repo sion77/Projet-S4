45a9d60a742b5bf6990491e1ec6371ee6dad49fc
Julien Rene <julien.rene@u-psud.fr>
2012-02-22 10:36:18 +0100

    Un tableau des id de menu d'un restaurant

---
+++ Projet-s4/AlgoAleatoires/tousLesMenusDUnResto.php
@@ -0,0 +1,25 @@
+<?php
+	/* Renvoie un tableau contenant les menus d'un restaurant
+	* Auteur Julien
+	*/
+	function tousLesMenusDUnResto($unResto) {
+		//Connexion à la base de données
+		$idConnexion = connexionDB();
+		
+		$requete = "SELECT idMenu FROM menurestaurant WHERE idRestaurant = " . $unResto, $idConnexion);
+		if(!requete)
+			die("Requête invalide : " . mysql_error());
+		else {
+			//Création du tableau
+			$lesMenus = array();
+			
+			//Remplissage du tableau
+			while ($unMenu = mysql_fetch_array($requete)) {
+				$lesMenus[] = $unMenu['idMenu'];
+			}
+			
+			return $lesMenus;
+		}
+	}
+?>
+				
