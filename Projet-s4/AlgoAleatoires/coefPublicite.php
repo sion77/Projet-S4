3a27e8a1480293472594407cd64b031d239b3c42
Julien Rene <julien.rene@u-psud.fr>
2012-02-22 09:49:14 +0100

    Update coefPublicite.php. Fonctionne, necessite un equilibrage

--- Projet-s4/AlgoAleatoires/coefPublicite.php
+++ Projet-s4/AlgoAleatoires/coefPublicite.php
@@ -1,33 +1,25 @@
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr">
-	<head>
-		<meta http-equiv="Content-Type" content="application/xhtml+xml; charset=ISO-8859-1" />
-		<title>Bonjour</title>
-	</head>
-	<body>
-		<!-- DEBUT DE CODE -->
-		<p><?php
+<?php
 	/* Calcule le coefficient de publicite (supérieure à 0.8) en fonction des dépenses en publicité. Cette valeur intervient dans la note du restaurant.
 	* Auteur Julien
 	*/
-	//function coefPublicite($unResto){
+	function coefPublicite($unResto){
 		//Le coefficient aura une valeur initiale pour les restaurants n'ayant pas dépensé de l'argent dans la publicité.
 		//Etant inférieur à 1, il s'agit donc d'un malus si aucune communication n'est faite.
 		//Il s'agit aussi d'une valeur par défaut s'il y a une erreur dans la requête.
 		$coefPublicite = 0.3;
 		$depenses = 0;
 		$n = 1;
-		//$idConnexion = connexionDB();
+		$idConnexion = connexionDB();
 		
 		//On récupère la valeur de la dépense en communication du restaurant
 		//$requete = mysql_query("SELECT publicite FROM restaurant WHERE id = " . $unResto, $idConnexion);
-		//if (!$requete)
-			//die("Requête invalide : " . mysql_error());
-		//else {
-			//$depenses = mysql_fetch_row($requete);
-			$depenses = 70000;
-			
+		if (!$requete)
+			die("Requête invalide : " . mysql_error());
+		else {
+			$depenses = mysql_fetch_row($requete);
+						
 			$coefPublicite = 0;
-			//
+			//On récupère la partie entière des dépenses divisée par 100, pour déterminer l'exposant n dans la suite 1/x^n.
 			$n = floor($depenses / 1000);
 			
 			//Calculs successifs du coefficient. L'efficacité des dépenses en publicité diminue à chaque itération de la boucle.
@@ -36,10 +28,6 @@
 			}
 			
 			echo $coefPublicite;
-		//}
-	//}
+		}
+	}
 ?>
-		</p>
-		<!-- FIN DE CODE ?-->
-	</body>
-</html>
