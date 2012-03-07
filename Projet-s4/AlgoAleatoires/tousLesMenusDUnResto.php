2fd505b71c146358e4d98347457e4f2952ea6036
Julien Rene <julien.rene@u-psud.fr>
2012-03-07 11:46:37 +0100

    Update tousLesMenusDUnResto.php

--- Projet-s4/AlgoAleatoires/tousLesMenusDUnResto.php
+++ Projet-s4/AlgoAleatoires/tousLesMenusDUnResto.php
@@ -3,22 +3,29 @@
 	* Auteur Julien
 	*/
 	function tousLesMenusDUnResto($unResto) {
-		//Connexion à la base de données
-		$idConnexion = connexionDB();
-		
-		$requete = mysql_query("SELECT idMenu FROM menurestaurant WHERE idRestaurant = " . $unResto, $idConnexion);
-		if(!requete)
-			die("Requête invalide : " . mysql_error());
+		// Etapes de connexion avec vérifications
+		$idConnexion = mysql_connect('localhost', 'root', '');
+		if(!$idConnexion)
+			echo "Attention : problème de connexion avec le serveur.";
 		else {
-			//Création du tableau
-			$lesMenus = array();
-			
-			//Remplissage du tableau
-			while ($unMenu = mysql_fetch_array($requete)) {
-				$lesMenus[] = $unMenu['idMenu'];
+			$connexionReussie = mysql_select_db('restaugame', $idConnexion);
+			if(!$connexionReussie)
+				echo "Attention : problème de connexion à la base de données.";
+		
+			$requete = mysql_query("SELECT idMenu FROM menurestaurant WHERE idRestaurant = " . $unResto, $idConnexion);
+			if(!requete)
+				die("Requête invalide : " . mysql_error());
+			else {
+				//Création du tableau
+				$lesMenus = array();
+				
+				//Remplissage du tableau
+				while ($unMenu = mysql_fetch_array($requete)) {
+					$lesMenus[] = $unMenu['idMenu'];
+				}
+				
+				return $lesMenus;
 			}
-			
-			return $lesMenus;
 		}
 	}
 ?>
