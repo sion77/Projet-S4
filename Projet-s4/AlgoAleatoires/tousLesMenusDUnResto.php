66c5b6444d31dd34c3c19327c489e5ecc4b8a900
Julien Rene <julien.rene@u-psud.fr>
2012-03-20 11:56:47 +0100

    Update tousLesMenusDUnResto.php

--- Projet-s4/AlgoAleatoires/tousLesMenusDUnResto.php
+++ Projet-s4/AlgoAleatoires/tousLesMenusDUnResto.php
@@ -3,20 +3,20 @@
 	* Auteur Julien
 	*/
 	function tousLesMenusDUnResto($unResto) {
-		// Etapes de connexion avec v�rifications
+		// Etapes de connexion avec vérifications
 		$idConnexion = mysql_connect('localhost', 'root', '');
 		if(!$idConnexion)
-			echo "Attention : probl�me de connexion avec le serveur.";
+			echo "Attention : problème de connexion avec le serveur.";
 		else {
 			$connexionReussie = mysql_select_db('restaugame', $idConnexion);
 			if(!$connexionReussie)
-				echo "Attention : probl�me de connexion � la base de donn�es.";
+				echo "Attention : problème de connexion � la base de donn�es.";
 		
 			$requete = mysql_query("SELECT idMenu FROM menurestaurant WHERE idRestaurant = " . $unResto, $idConnexion);
 			if(!$requete)
 				die("Requ�te invalide : " . mysql_error());
 			else {
-				//Cr�ation du tableau
+				//Création du tableau
 				$lesMenus = array();
 				
 				//Remplissage du tableau
